08a52361587873c16bf5f13e806734309f16a05f
yixiaoshan <yixiaoshan@xuetangx.com>
2016-05-18 16:45:13 +0800

    babel set up

---
+++ src/index.js
@@ -0,0 +1,11 @@
+import 'babel-polyfill';
+console.log(Array.from('aaaa'));
+
+console.log("====");
+alert(222);
+
+const sum = (a,b) => a+b;
+let x = sum(1,2);
+console.log(x);
+
+Object.assign()
---
+++ src/jsx.js
@@ -0,0 +1,7 @@
+import React, {Component} from 'react';
+
+class DemoComponent extends Component{
+	render(){
+		return <h1>hello world</h1>
+	}
+}
